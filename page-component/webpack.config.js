bbda574e2a73e2fc40025e5129fb17d05e8bba02
sunxianfeng <sunxianfeng@jiuqi.com.cn>
2019-01-22 10:46:40 +0800

    删除打包源码

--- page-component/webpack.config.js
+++ page-component/webpack.config.js
@@ -9,7 +9,6 @@ less.render(fs.readFileSync(path.join(__dirname, "src/page.less")).toString('utf
 
 module.exports = {
     entry: "./src/page.js",
-    devtool: 'eval-source-map',
     output: {
         path: path.join(__dirname, "dist"),
         filename: "page.js",
